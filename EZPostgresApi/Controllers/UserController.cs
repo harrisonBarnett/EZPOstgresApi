3f61d0d0b2eaa929ae4836959ae53efbf72ecb40
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-05-31 16:55:41 -0400

    initial commit

---
+++ EZPostgresApi/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Npgsql;
+using System.Data;
+
+namespace EZPostgresApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        NpgsqlConnection myConn;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            string connString = _configuration.GetConnectionString("DefaultConnection");
+            myConn = new NpgsqlConnection(connString);
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string query = "SELECT * FROM users";
+
+            DataTable table = new DataTable();
+            NpgsqlDataReader reader;
+            myConn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand(query, myConn);
+            reader = cmd.ExecuteReader();
+            table.Load(reader);
+
+            reader.Close();
+            myConn.Close();
+
+            return JsonConvert.SerializeObject(table);
+        }
+    }
+}
---
+++ EZPostgresApi/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// cors configuration bc it will piss you off later
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
